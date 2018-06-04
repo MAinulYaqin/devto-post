6b68c588b2c1ff9eea3b3d1cd798c5c3dd2c7e3a
MAinulYaqin <ainul.y9b@gmail.com>
2018-06-04 14:06:22 +0700

    auto login to github then take a screenshot

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const puppeteer = require('puppeteer');
 
+let config = require('./config/user.js');
+
+// DOM selector
+const USERNAME_SELECTOR = '#login_field';
+const PASSWORD_SELECTOR = '#password';
+const BUTTON_SELECTOR = '#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block';
+
+// Search
+const userToSearch = 'MAinulYaqin'
+const searchUrl = `https://github.com/search?q=${userToSearch}&type=Users&utf8=%E2%9C%93`;
+
 async function run () {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: true
+    });
     const page = await browser.newPage();
 
-    await page.goto('https://www.github.com/MAinulYaqin');
+    await page.goto('https://www.github.com/login');
+
+    await page.click(USERNAME_SELECTOR);
+    await page.keyboard.type(config.username);
+
+    await page.click(PASSWORD_SELECTOR);
+    await page.keyboard.type(config.password);
+
+    await page.click(BUTTON_SELECTOR);
+
+    await page.waitForNavigation();
+
     await page.screenshot({path: './github.png'})
+    console.log('success')
 }
 
 run()
