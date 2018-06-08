f3aa0066774323d696c80b128e19235ada433ec7
MAinulYaqin <ainul.y9b@gmail.com>
2018-06-08 14:34:58 +0700

    refactoring and remove useless file

--- devto.js
+++ devto.js
@@ -1,6 +1,13 @@
 const puppeteer = require('puppeteer');
 const config = require('./config/user');
 
+const SUBSTORIES_DOM = ".single-article.single-article-small-pic";
+const TITLE_SUBSTORIES = `${SUBSTORIES_DOM}:nth-of-type(INDEX) > a > .content > h3`;
+const LINK_SUBSTORIES = `${SUBSTORIES_DOM}:nth-of-type(INDEX) > .small-pic-link-wrapper.index-article-link`;
+
+let titleSelector = TITLE_SUBSTORIES.replace('INDEX', i)
+let linkSelector = LINK_SUBSTORIES.replace('INDEX', i)
+
 async function run() {
     const browser = await puppeteer.launch({
         headless: true
@@ -10,17 +17,11 @@ async function run() {
     await page.goto('https://dev.to');
     // await page.screenshot({path: './devto.png'});
 
-    const SUBSTORIES_DOM = ".single-article.single-article-small-pic";
-    const TITLE_SUBSTORIES = `${SUBSTORIES_DOM}:nth-of-type(INDEX) > a > .content > h3`;
-    const LINK_SUBSTORIES = `${SUBSTORIES_DOM}:nth-of-type(INDEX) > .small-pic-link-wrapper.index-article-link`
-
     let listLength = await page.evaluate((sel) => (
         document.querySelectorAll(sel).length
     ), SUBSTORIES_DOM)
 
     for (let i = 1; i <= listLength; i++) {
-        let titleSelector = TITLE_SUBSTORIES.replace('INDEX', i)
-        let linkSelector = LINK_SUBSTORIES.replace('INDEX', i)
 
         let title = await page.evaluate((sel) => {
             if (!document.querySelector(sel)) return '\n\n\n =========== nothing to see :( ==========';
